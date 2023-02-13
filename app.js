045f6ea1242ef7efeb72a2e1e8ae185897c594ff
nkhlrgh <43105834+nkhlrgh@users.noreply.github.com>
2023-02-13 14:29:38 +0530

    feat: generate and display random computer choice

--- app.js
+++ app.js
@@ -3,8 +3,24 @@ const userChoiceDisplay = document.getElementById('user-choice');
 const resultDisplay = document.getElementById('result-choice');
 const possibleChoices = document.querySelectorAll('button');
 let userChoice;
+let computerChoice;
 
-possibleChoices.forEach(possibleChoice => addEventListener('click', (e) => {
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     userChoice = e.target.id;
     userChoiceDisplay.innerHTML = userChoice;
+    getComputerChoice();
 }))
+
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1;
+    if(randomNumber === 1) {
+        computerChoice = "Rock";
+    }
+    if(randomNumber === 2) {
+        computerChoice = "Paper";
+    }
+    if(randomNumber === 3) {
+        computerChoice = "Scissors";
+    }
+    computerChoiceDisplay.innerHTML = computerChoice;
+}
